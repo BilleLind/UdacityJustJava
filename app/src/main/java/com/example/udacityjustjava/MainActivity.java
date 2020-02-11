ffc069f64ce56ee95ebce4539dba7e13fba5024a
yokarak <ande67f3@edu.easj.dk>
2020-02-11 13:56:58 +0100

    works

--- app/src/main/java/com/example/udacityjustjava/MainActivity.java
+++ app/src/main/java/com/example/udacityjustjava/MainActivity.java
@@ -1,14 +1,71 @@
 package com.example.udacityjustjava;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
+   static int numberOfCoffees;
+    public int getNumberOfCoffees() {
+        return numberOfCoffees;
+    }
+    public void setNumberOfCoffees(int numberOfCoffees) {
+        this.numberOfCoffees = numberOfCoffees;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(numberOfCoffees);
+        displayPrice(numberOfCoffees*20);
+    }
+
+    public void increment(View view) {
+    setNumberOfCoffees(getNumberOfCoffees()+1);
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_number);
+    quantityTextView.setText("" + numberOfCoffees);
+    }
+    public void decrement(View view) {
+        if (getNumberOfCoffees()==0) {System.out.println("Error, at 0 coffees");
+        } else { setNumberOfCoffees(getNumberOfCoffees()-1);
+            TextView quantityTextView = (TextView) findViewById(
+                    R.id.quantity_number);
+        quantityTextView.setText("" + numberOfCoffees);}
+    }
+
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display( int numberOfCoffees) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_number);
+        quantityTextView.setText("" + numberOfCoffees);
+    }
+    /**
+     * This method displays the given price on the screen.
+     */
+    private void displayPrice(int numberOfCoffees) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(numberOfCoffees));
+    }
+
+
+
+
 }
