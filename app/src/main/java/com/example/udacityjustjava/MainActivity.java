780b995ca0dd02559349cb1053926ecba98c9943
yokarak <ande67f3@edu.easj.dk>
2020-02-15 00:42:34 +0100

    opens a mail with the subject

--- app/src/main/java/com/example/udacityjustjava/MainActivity.java
+++ app/src/main/java/com/example/udacityjustjava/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.udacityjustjava;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.util.Log;
 import android.view.View;
 import android.os.Bundle;
@@ -30,12 +31,15 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+
         display(numberOfCoffees);
         displayPrice(createOrderSummary(calculatePrice())+ "Thank you!"); //adding Thank you here, so it isn't displayed while "Browsing"
 
 
-        Intent intent = new Intent(Intent.ACTION_SEND);
-        intent.setData()
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only mail apps should handle this
+        //intent.putExtra(Intent.EXTRA_EMAIL, addresses); should be the one if there
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java Order for " +name());
 
         if (intent.resolveActivity(getPackageManager())!=null) {
             startActivity(intent);
@@ -44,7 +48,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void increment(View view) { // have added the creatOrderSummary here so it is updated for every click
 
-        if (numberOfCoffees<24) {System.out.println("Error - to much Coffee!!"); }
+        if (numberOfCoffees==24) {System.out.println("Error - to much Coffee!!"); }
         else { setNumberOfCoffees(getNumberOfCoffees()+1);}
     display(numberOfCoffees);
     displayPrice(createOrderSummary(calculatePrice()));
@@ -94,17 +98,20 @@ public class MainActivity extends AppCompatActivity {
             checkBox +="Add chocolate\n";
             price+=(5*numberOfCoffees); }
         //connecting the input for
-        EditText nameEditText = findViewById(R.id.editText_name);
-        //i am using the .getText() to get the input from the EditText into the String that is displayed. calling it in the String instead of casting it into a
-        //String and then using the String in the message below
-        String priceMessage = "Name: " + nameEditText.getText() + "\n" +
+
+
+        String priceMessage = "Name: " + name() + "\n" +
                 checkBox + // since i am using += above, and \n the different toppings is added in here and are going to the next line and their value is displayed
                 "Quantity: " + numberOfCoffees +
                 "\nTotal: " + price + " kr.\n";
 
         return priceMessage; } //could delete this and move the return up before the "String priceMessage, but this is more clean in a way
 
-
+        private String name() { // since i needed the name else where (in the email subject) i made it like this, more of a niche thing
+            EditText nameEditText = findViewById(R.id.editText_name);
+            String name = nameEditText.getText().toString();
+            return name;
+        }
 
 
 }
